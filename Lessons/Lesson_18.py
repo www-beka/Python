363c2229bfdcf3248a366e9521963fd7420b8696
Bekzod <bekmuradovbekzod955@gmail.com>
2023-06-22 14:22:41 +0500

    lesson 18 and 19

--- Lessons/Lesson_18.py
+++ Lessons/Lesson_18.py
@@ -9,29 +9,43 @@
 # print(ff.readline())
 # ff.close()
 
-def is_prime(num):
-    if num <= 1:
-        return False
-    elif num <= 3:
-        return True
-    elif num % 2 == 0 or num % 3 == 0:
-        return False
-    i = 5
+# def is_prime(num):
+#     if num <= 1:
+#         return False
+#     elif num <= 3:
+#         return True
+#     elif num % 2 == 0 or num % 3 == 0:
+#         return False
+#     i = 5
     
-    while i*i <= num:
-        if num % i == 0 or num % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+#     while i*i <= num:
+#         if num % i == 0 or num % (i + 2) == 0:
+#             return False
+#         i += 6
+#     return True
 
-count = 0
-num = 2
+# count = 0
+# num = 2
 
-while count < 100:
-    if is_prime(num):
-        print(num)
-        count += 1
-    num += 1
+# while count < 100:
+#     if is_prime(num):
+#         print(num)
+#         count += 1
+#     num += 1
 
 
-print(is_prime(101))
+# print(is_prime(101))
+
+
+# Task 1: Write a program that asks the user to enter a number and checks if the number is even or odd. If it's even, print "The number is even." If it's odd, print "The number is odd."
+num = int(input("Enter a number"))
+if num % 2 == 0: 
+    print("The number is even.")
+else:
+    print("The number is odd." )
+
+# Example Input: Enter a number: 6
+# Example Output: The number is even.
+# Task 2: Write a program that asks the user to enter two numbers and checks if the first number is greater than, less than, or equal to the second number. If it's greater than, print "The first number is greater than the second." If it's less than, print "The first number is less than the second." If it's equal to, print "The first number is equal to the second."
+# Example Input: Enter the first number: 10 Enter the second number: 5
+# Example Output: The first number is greater than the second.
---
+++ Lessons/Lesson_19.py
@@ -0,0 +1,91 @@
+# ===============================================================================
+
+# 10. Write a program that prints out the first 100 prime numbers.-  
+# RU: Напишите программу, которая выводит первые 100 простых чисел.
+def n_prime_numbers(n):
+    pass
+
+
+# ===============================================================================
+
+# Write a program that calculates the sum of the first 1000 Fibonacci numbers.
+# Write a program that prints out the first 1000 digits of pi (3.14).
+# Write a program that generates a random password of length 20.
+# Write a program that calculates the factorial of a given number.
+# Write a program that prints out the first 1000 perfect squares.
+# Write a program that generates a random maze and solves it using the A* algorithm.
+# Write a program that calculates the sum of the first 1000 prime numbers.
+# Write a program that generates a random Sudoku puzzle and solves it.
+# Write a program that calculates the sum of the first 1000 triangular numbers.
+
+# ===============================================================================
+
+
+
+# if __name__ == "__main__":
+#     print(pattern(int(input("Enter a number: "))))
+
+# ===============================================================================
+# ===============================================================================
+
+# num = range(1500, 2701)
+# for i in num:
+#     if (i % 7 == 0) and (i % 5 == 0):
+#         print(i)
+    
+# ===============================================================================
+
+
+# n=5;
+# for i in range(n):
+#     for j in range(i):
+#         print ('* ', end="")
+#     print('')
+
+# for i in range(n,0,-1):
+#     for j in range(i):
+#         print('* ', end="")
+#     print('')
+	
+# ===============================================================================
+# ===============================================================================
+
+# even = []
+# odd = []
+
+# numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+# for i in numbers:
+#     if i % 2 == 0:
+#         even.append(i)
+#     else:
+#         odd.append(i)
+
+
+# print(f"Even : {len(even)}", f"Odd : {len(odd)}")
+
+# ===============================================================================
+
+# datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12], {"class": 'V', "section": 'A'} ]
+
+# for i in datalist:
+#     print(type(i))
+
+# ===============================================================================
+
+
+# i = 0
+# while i < 6:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+# ===============================================================================
+
+x,y = 0,1
+
+while y < 50:
+    print(y)
+    x,y = y,x + y
+
+# ===============================================================================
